5a4b661314fa9f234e3bf2e9467c9d0572719b4c
anacristinaleonh-6 <anacristinaleonh@gmail.com>
2026-06-20 06:41:45 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.title("🐟 Highland Fish Dashboard")
+
+st.metric("Ventas Totales","S/ 287,529")
+st.metric("Ganancia","S/ 132,221")
+st.metric("Clientes","9")
+st.metric("Trabajadores","7")
